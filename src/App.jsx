05c16aba9f10501b95ae7af26b125c61e727cbcb
Devsi-git <yousefi.devsi@gmail.com>
2025-05-28 05:23:02 +0330

    UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route element={<AppLayout />}>
           <Route index element={<Navigate replace to="home" />} />
           <Route path="home" element={<Home />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
